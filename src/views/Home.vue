2e383bac8d45ae7874482cf3ef89c59abf42e891
SungmanYou <sungman.you@gmail.com>
2019-06-05 20:38:45 +0900

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img
       alt="Vue logo"
-      src="../assets/images/logo.png"
+      src="@/assets/images/logo.png"
     />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -14,6 +14,10 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'ViewHome',
+  data() {
+    return {};
+  },
+  methods: {},
   components: {
     HelloWorld
   }

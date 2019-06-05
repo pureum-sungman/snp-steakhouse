44e5e70abdcf4f237ac9d2433e0f376587259b2a
SungmanYou <sungman.you@gmail.com>
2019-06-06 00:24:37 +0900

    구글 폰트 & 반응형 글씨 크기 & 부트스트랩4 설정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,27 @@
       src="@/assets/images/logo.png"
     />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>
+      반응형 글씨 크기 h1
+    </h1>
+    <h2>
+      반응형 글씨 크기 h2
+    </h2>
+    <h3>
+      반응형 글씨 크기 h3
+    </h3>
+    <h4>
+      반응형 글씨 크기 h4
+    </h4>
+    <h5>
+      반응형 글씨 크기 h5
+    </h5>
+    <h6>
+      반응형 글씨 크기 h6
+    </h6>
+    <p class="mb-5">
+      반응형 글씨 크기 p
+    </p>
   </div>
 </template>
 

aa064ba07868dfc10525a05cd3009889bdf05705
SungmanYou <sungman.you@gmail.com>
2019-06-06 09:58:51 +0900

    eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,29 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/strongly-recommended',
-    'prettier',
-    'prettier/vue'
-  ],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  globals: {
-    'ga': true, // Google Analytics
-    'process': true,
-    'module': true
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: [
+        'plugin:vue/strongly-recommended',
+        'prettier',
+        'prettier/vue'
+    ],
+    plugins: [
+        'vue', 'prettier'
+    ],
+    rules: {
+        "prettier/prettier": "error",
+        "vue/max-attributes-per-line": 0,
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    },
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+    globals: {
+        'ga': true, // Google Analytics
+        'process': true,
+        'module': true
+    }
 };

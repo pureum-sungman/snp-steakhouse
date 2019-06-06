51d82cab45b50972eba80d4c2c77e915988cc54a
SungmanYou <sungman.you@gmail.com>
2019-06-06 09:23:53 +0900

    반응형 레이아웃 추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 
-// Vue 플러그인
+// 뷰 플러그인
 import router from '@/plugins/vue-router';
 import store from '@/plugins/vuex';
 import '@/plugins/axios';
@@ -13,12 +13,14 @@ import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 
+// 뷰 설정
 Vue.config.productionTip = false;
 
+// 부트스트랩 4 'sm' 사이즈 기준으로 구분 ( 임시 )
 const layout = window.innerWidth <= 576 ? 'Mobile' : 'Desktop';
-import(`@/views/layouts/${layout}.vue`)
-    .then(App => new Vue({
-        router,
-        store,
-        render: h => h(App.default)
-    }).$mount('#app'));
+
+import(`@/views/layouts/${layout}.vue`).then(module => new Vue({
+    router,
+    store,
+    render: h => h(module.default)
+}).$mount('#app'));

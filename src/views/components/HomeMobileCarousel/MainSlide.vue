012a45d1420cc2dd22e4360482c9c6973c8d504b
SungmanYou <sungman.you@gmail.com>
2019-06-08 07:57:19 +0900

    홈 컴포넌트 - 모바일 슬라이더

--- src/views/components/HomeMobileCarousel/MainSlide.vue
+++ src/views/components/HomeMobileCarousel/MainSlide.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
-    CAROUSEL!!
-  </div>
+  <div class="swiper-slide"></div>
 </template>
 
 <script>
 export default {};
 </script>
+
 <style lang="scss" scoped></style>
--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -4,7 +4,8 @@
       Carosuel: BEGIN 
       1. Vuex의 현재 'isMobile' 값에 따라서 디스플레이 여부 결정
     -->
-    <home-mobile-carousel v-show="$store.state.isMobile" />
+    <!-- <home-mobile-carousel v-if="!$store.state.isMobile" /> -->
+    <home-mobile-carousel v-if="true" />
     <!-- Carosuel: END -->
   </div>
 </template>
@@ -13,7 +14,7 @@
 export default {
   name: 'PageHome',
   components: {
-    HomeMobileCarousel: () => import('@/views/components/HomeMobileCarousel.vue') // prettier-ignore
+    HomeMobileCarousel: () => import('@/views/components/HomeMobileCarousel/Index.vue') // prettier-ignore
   }
 };
 </script>

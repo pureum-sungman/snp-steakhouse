2f45a8c6e956d08d98aa36168e54321acc2b687d
SungmanYou <sungman.you@gmail.com>
2019-06-07 19:51:51 +0900

    홈 섹션 컴포넌트 - 레시피

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,30 +1,6 @@
 <template>
   <div page="home">
-    <section id="recipes">
-      <div class="row">
-        <div class="col text-center">
-          <h1 class="font-cursive display-1">
-            Recipes
-          </h1>
-        </div>
-        <!-- .col -->
-      </div>
-      <!-- .row -->
-      <div class="row">
-        <div
-          v-for="(recipe, index) in recipes"
-          :key="index"
-          class="col-12 col-md-6 col-lg-4"
-        >
-          <h1 class="font-cursive">
-            {{ recipe.title }}
-          </h1>
-        </div>
-        <!-- .col-12 col-md-6 col-lg-4 -->
-      </div>
-      <!-- .row -->
-    </section>
-    <!-- #recipes -->
+    <home-section-recipes />
   </div>
   <!-- .container -->
 </template>
@@ -32,15 +8,8 @@
 <script>
 export default {
   name: 'PageHome',
-  data() {
-    return {
-      recipes: [{ title: 'A' }, { title: 'B' }, { title: 'C' }]
-    };
-  },
-  mounted() {
-    this.$nextTick(() => {});
-  },
-  methods: {},
-  components: {}
+  components: {
+    HomeSectionRecipes: () => import('@/views/components/HomeSectionRecipes.vue') // prettier-ignore
+  }
 };
 </script>

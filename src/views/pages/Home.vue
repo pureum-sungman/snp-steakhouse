923f16d4525cc7d01024c729dd43cd1f3fc91690
SungmanYou <sungman.you@gmail.com>
2019-06-08 07:27:57 +0900

    홈 컴포넌트 - 모바일 슬라이더

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div page="home">
+    <!-- 
+      Carosuel: BEGIN 
+      1. Vuex의 현재 'isMobile' 값에 따라서 디스플레이 여부 결정
+    -->
     <home-mobile-carousel v-show="$store.state.isMobile" />
+    <!-- Carosuel: END -->
   </div>
 </template>
 

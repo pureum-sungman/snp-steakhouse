28385f4722b9287162da7a9d333e34e155cae5a9
SungmanYou <sungman.you@gmail.com>
2019-06-07 17:20:25 +0900

    Vuex - Strict 모드

--- src/plugins/vuex/index.js
+++ src/plugins/vuex/index.js
@@ -6,6 +6,7 @@ import recipes from '@/plugins/vuex/recipes.module';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
   modules: {
     recipes
   }

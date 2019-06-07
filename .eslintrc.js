e8ccef9a44a2ac924579bafa7ea3bed345f01173
SungmanYou <sungman.you@gmail.com>
2019-06-07 17:17:17 +0900

    jQuery 글로벌 스코프

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    jquery: true
   },
   extends: ['plugin:vue/strongly-recommended', '@vue/prettier'],
   rules: {
@@ -13,7 +14,6 @@ module.exports = {
     parser: 'babel-eslint'
   },
   globals: {
-    'process': true,
-    'module': true
+    'process': true
   }
 };

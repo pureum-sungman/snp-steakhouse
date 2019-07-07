dbbeccbf689f775d5413f987d78b6b9b15ed2dd6
SungmanYou <sungman.you@gmail.com>
2019-07-07 19:35:51 +0900

    반응형 컴포넌트를 위한 플러그인 추가

---
+++ src/plugins/vue-responsive-components.js
@@ -0,0 +1,15 @@
+/**
+ * Vue Responsive Components
+ * @link https://github.com/Kelin2025/vue-responsive-components
+ *
+ * 윈도우 크기에 의존하지 않고,
+ * 각각의 컴포넌트의 크기를 기준으로 하는
+ * 독립적인 반응형 컴포넌트 제작을 위한 플러그인
+ *
+ * 사용 예는 링크 참조
+ **/
+
+import Vue from 'vue';
+import { VueResponsiveComponents } from 'vue-responsive-components';
+
+Vue.use(VueResponsiveComponents);

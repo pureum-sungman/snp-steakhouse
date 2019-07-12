93b3a0f6f019f0a0676eb85926f3cccb948a41b5
dudxhd <dudxhddiehd@naver.com>
2019-07-12 19:57:44 +0900

    7/12

--- src/views/pages/About.vue
+++ src/views/pages/About.vue
@@ -1,11 +1,10 @@
 <template>
   <div page="about">
     <div class="about">
-      <div class="map">
-        <div class="text"></div>
-      </div>
+      <div class="info"></div>
       <div class="section">
-        <div class="info"></div>
+        <div class="map"></div>
+        <div class="text"></div>
       </div>
     </div>
   </div>
@@ -17,14 +16,14 @@ export default {
   mounted() {
     this.$nextTick(() => {
 
-      $('.map').mouseover(function (e) { 
-          $('.map').addClass('view');
+      $('.section').mouseover(function (e) { 
           $('.section').addClass('on');
+          $('.map').addClass('view');
           $('.text').addClass('ii');
       });
-      $('.map').mouseout(function (e) { 
-          $('.map').removeClass('view');
+      $('.section').mouseout(function (e) { 
           $('.section').removeClass('on');
+          $('.map').removeClass('view');
           $('.text').removeClass('ii');
       });
     });
@@ -40,6 +39,7 @@ export default {
     height: 100vh;
   }
   .info{
+    position: absolute;
     width: 100%;
     height: 100%;
     background: url('https://cdn.pixabay.com/photo/2015/03/30/12/37/jellyfish-698521__340.jpg') no-repeat;
@@ -48,43 +48,49 @@ export default {
   .section{
     position: absolute;
     overflow: hidden;
-    top: -60%;
-    left: -50%;
-    width: 150%;
-    height: 200%;
-    background-color: green;
-    transform: rotate(-40deg);
-    transition: 1.5s all ease-out;
-  }
-  .on{
-    top: -90%;
-    left: -56%;
-  }
-  .map{
-    position: absolute;
     bottom: 0;
     right: 0;
+    width: 10%;
+    height: 20%;
+    background-color: green;
+    transition: 1.5s all ease-out;
+  }
+  .section.on{
     width: 50%;
-    height: 80%;
-    background-color: yellow;
-    transition: 1s all ease-out;
+    height: 60%;
   }
-  .text{
-    position: absolute;
-    bottom: 5%;
-    right: 3%;
-    width: 35%;
-    height: 30%;
-    transition: 1.5s all linear;
-    border: 1px solid black;
-  }
-  .view{
-    width: 70%;
-    height: 100%;
-    background-color: red;
-  }
-  .ii{
-    width: 45%;
-    height: 40%;
-  }
+ .map{
+   position: absolute;
+   top: 5%;
+   left: 50%;
+   width: 90%;
+   height: 90%;
+   border: 1px solid black;
+   transform: translateX(-50%);
+   background: blue;
+   transition: 1s all ease-out;
+ }
+ .map.view{
+   position: absolute;
+   top: 10%;
+   left: 50%;
+   width: 80%;
+   height: 60%;
+   border: 1px solid black;
+   transform: translateX(-50%);
+   background: yellow;
+ }
+ .text{
+   display: none;
+   position: absolute;
+   bottom: 10%;
+   left: 50%;
+   width: 80%;
+   height: 15%;
+   transform: translateX(-50%);
+   border: 1px solid black;
+ }
+ .text.ii{
+   display: block;
+ }
 </style>
